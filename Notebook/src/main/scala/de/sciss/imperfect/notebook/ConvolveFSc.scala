cbc1b2b70bed1ecdd419b6aed84794205e6fb1b5
Hanns Holger Rutz <contact@sciss.de>
2016-10-23 00:06:34 +0200

    have a program but it's stuck

--- Notebook/src/main/scala/de/sciss/imperfect/notebook/ConvolveFSc.scala
+++ Notebook/src/main/scala/de/sciss/imperfect/notebook/ConvolveFSc.scala
@@ -22,56 +22,139 @@ import scala.Predef.{any2stringadd => _, _}
 import scala.swing.Swing
 
 object ConvolveFSc {
-  final case class Config(kernel: Int = 16, noiseAmp: Double = 0.1, width: Int = 1024, height: Int = 1024)
+  final case class Config(kernel: Int = 16, noiseAmp: Double = 0.1, width: Int = 1024, height: Int = 1024,
+                          groupIdx: Int = 1, fadeFrames: Int = 24 /* * 14 */)
 
   def main(args: Array[String]): Unit = run(Config())
 
+  case class Levels(r: (Int, Int), g: (Int, Int), b: (Int, Int), value: (Int, Int))
+
+  def applyLevels(in: GE, levels: Levels): GE = {
+    import levels._
+    val chans = r :: g :: b :: Nil
+    val rgb   = Seq(in \ 0, in \ 1, in \ 2): GE
+//    val alpha = in \ 3
+    val low   = chans.map(tup => tup._1 / 255.0: GE)
+    val high  = chans.map(tup => tup._2 / 255.0: GE)
+    val p1    = rgb.linlin(low, high, 0, 1)
+    val p2    = p1 .linlin(value._1 / 255.0, value._2 / 255.0, 0, 1)
+    val clip  = p2 .max(0).min(1).elastic(10)
+    clip // Seq(clip \ 0, clip \ 1, clip \ 2 /* , alpha */): GE
+  }
+
+  def invert(in: GE): GE = (-in + (1: GE)).elastic(2)
+
   def run(config: Config): Unit = {
     import config._
-    val dir       = userHome / "Documents" / "projects" / "Imperfect" / "scans" /"notebook2016"
-    val fIn1      = dir / "universe-test1.png"
-    val fIn2      = dir / "universe-test2.png"
-    val fFltIn    = dir / s"hp5-fft2d-$kernel.aif"
-    val fOut      = dir / "universe-fscape-out.png"
+    val baseDir       = userHome / "Documents" / "projects" / "Imperfect" / "scans" /"notebook2016"
+    val pagesDir      = baseDir / "universe-pages"
+    val groupInDir    = pagesDir / s"group-$groupIdx"
+    val numPages      = groupInDir.children(_.ext == "png").size
+    val outDir        = baseDir / "universe-out"
+    val groupOutDir   = outDir / s"group-$groupIdx"
+    groupOutDir.mkdirs()
+    val tempIn        = groupInDir  / "notebook-p%d.png"
+    val tempOut       = groupOutDir / "frame-%d.png"
+    val tempInRange   = 1 to numPages
+    val tempOutRange  = 1 to ((2 * numPages + 1) * fadeFrames)
 
-    if (fOut.exists() && fOut.length() > 0) {
-      println(s"File '${fOut.name}' already exists. Not overwriting")
-      sys.exit(1)
-    }
+    val fFltIn    = baseDir / s"hp5-fft2d-$kernel.aif"
+
+//    if (fOut.exists() && fOut.length() > 0) {
+//      println(s"File '${fOut.name}' already exists. Not overwriting")
+//      sys.exit(1)
+//    }
 
     var gui: SimpleGUI = null
     val cfg = Control.Config()
     cfg.useAsync = false
     cfg.progressReporter = p => Swing.onEDT(gui.progress = p.total)
 
+    def mkRangeGE(in: Seq[Int]): GE = in.map(x => x: GE).reduce(_ ++ _)
+
+    val lvl = Levels(r = 41 -> 254, g = 41 -> 254, b = 41 -> 254, value = 0 -> 200)
+
+    def adjustLevels(in: GE): GE = invert(applyLevels(in, lvl))
+
     val g = Graph {
       import graph._
-      val i1        = ImageFileIn(fIn1, numChannels = 3)
-      val i2        = ImageFileIn(fIn2, numChannels = 3)
-      val frameSize = width * height
+      val frameSize   = width * height
+      val (evenInRange, oddInRange) = tempInRange.partition(_ % 2 == 0)
+
+      def mkBlackFrame(): GE = DC(Seq[GE](0, 0, 0)).take(frameSize)
+
+      val imgSeqIn1   = ImageFileSeqIn(tempIn, indices = mkRangeGE(evenInRange), numChannels = 3)
+      val imgSeqIn2   = ImageFileSeqIn(tempIn, indices = mkRangeGE(oddInRange ), numChannels = 3)
+      val imgSeqIn1L  = adjustLevels(imgSeqIn1)
+      val imgSeqIn2L  = adjustLevels(imgSeqIn2)
+
+      val (inSeqRep1, inSeqRep2) = if (evenInRange.size < oddInRange.size) {
+        val _res1 = mkBlackFrame() ++ RepeatWindow(imgSeqIn1L, size = frameSize, num = 4) ++ mkBlackFrame()
+        val _res2 = RepeatWindow(imgSeqIn2L, size = frameSize, num = 4).drop(frameSize)
+          .take(frameSize.toLong * (oddInRange.size * 4 - 2))
+        (_res1, _res2)
+      } else {
+        ???
+      }
 
       val fltIn     = AudioFileIn(fFltIn, numChannels = 1)  // already FFT'ed
       val kernelS   = kernel * kernel
       val fltRepeat = RepeatWindow(fltIn, kernelS, num = frameSize)
 
-      val m1        = MatrixInMatrix(i1, rowsOuter = height, columnsOuter = width, rowsInner = kernel, columnsInner = kernel)
-      val m2        = MatrixInMatrix(i2, rowsOuter = height, columnsOuter = width, rowsInner = kernel, columnsInner = kernel)
+      val periodFrames = fadeFrames * 4
 
+      def mkSaw(phase: Double): GE = {
+        val lfSaw   = LFSaw(1.0/periodFrames, phase = phase)
+        val lfSawUp = (lfSaw + (1: GE)) * 2
+        val low     = lfSawUp.min(1) - (lfSawUp - 3).max(0)
+        val rep1    = RepeatWindow(low , size = 1, num = frameSize)
+        rep1
+      }
+
+      def mkSawMat(phase: Double): GE = {
+        val rep1    = mkSaw(phase)
+        val rep2    = RepeatWindow(rep1, size = 1, num = kernelS  )
+        rep2
+      }
+      
+      val env1Mat   = mkSawMat(0.75)
+      val env2Mat   = mkSawMat(0.25)
+
+      val m1        = MatrixInMatrix(inSeqRep1, rowsOuter = height, columnsOuter = width, rowsInner = kernel, columnsInner = kernel)
+      val m2        = MatrixInMatrix(inSeqRep2, rowsOuter = height, columnsOuter = width, rowsInner = kernel, columnsInner = kernel)
+
+      val scale1    = env1Mat.linexp(1, 0, 1, 0.01)
+      val scale2    = env2Mat.linexp(1, 0, 1, 0.01)
       val m1a       = AffineTransform2D.scale(in = m1, widthIn = kernel, heightIn = kernel,
-        sx = 0.2, sy = 0.2, zeroCrossings = 0, wrap = 0)
+        sx = scale1, sy = scale1, zeroCrossings = 0, wrap = 0)
+      val m2a       = AffineTransform2D.scale(in = m2, widthIn = kernel, heightIn = kernel,
+        sx = scale2, sy = scale2, zeroCrossings = 0, wrap = 0)
 
-      val m1n       = ResizeWindow(WhiteNoise(Seq[GE](1, 1, 1)), size = 1, start = 0, stop = kernelS - 1)
-      val m1x       = m1a + (m1n * 24 + 104)
+      val env1      = mkSaw(0.75)
+      val env2      = mkSaw(0.25)
+
+      val noiseAmp1 = env1.linlin(1, 0, 0,  24)
+      val noiseDC1  = env1.linlin(1, 0, 0, 104)
+      val noiseAmp2 = env2.linlin(1, 0, 0,  24)
+      val noiseDC2  = env2.linlin(1, 0, 0, 104)
+
+      val noise1    = WhiteNoise(Seq[GE](noiseAmp1, noiseAmp1, noiseAmp1)) + noiseDC1
+      val m1n       = ResizeWindow(noise1, size = 1, start = 0, stop = kernelS - 1)
+      val m1x       = m1a + m1n // (m1n * 24 + (104: GE))
+
+      val noise2    = WhiteNoise(Seq[GE](noiseAmp2, noiseAmp2, noiseAmp2)) + noiseDC2
+      val m2n       = ResizeWindow(noise2, size = 1, start = 0, stop = kernelS - 1)
+      val m2x       = m2a + m2n // (m1n * 24 + (104: GE))
 
       val m1f       = Real2FFT(m1x, rows = kernel, columns = kernel)
-      val m2f       = Real2FFT(m2 , rows = kernel, columns = kernel)
+      val m2f       = Real2FFT(m2x, rows = kernel, columns = kernel)
 
       val m3f       = (m1f.complex * m2f).complex * fltRepeat
       val m3        = Real2IFFT(m3f, rows = kernel, columns = kernel)
       val flt       = ResizeWindow(m3, size = kernelS, stop = -(kernelS - 1))
       val i3        = flt
 
-      Progress(Frames(i3) / (2 * frameSize), Metro(width), label = "ifft")
+//      Progress(Frames(i3) / (2 * frameSize), Metro(width), label = "ifft")
 
       val frameTr1  = Metro(frameSize)
       val frameTr2  = Metro(frameSize)
@@ -87,8 +170,9 @@ object ConvolveFSc {
 
       val sig       = i4
       val specOut   = ImageFile.Spec(width = width, height = height, numChannels = 3)
-      ImageFileOut(fOut, specOut, in = sig)
-      Progress(Frames(sig) / (2 * frameSize), Metro(frameSize), label = "write")
+      val tempOutRangeGE = Frames(DC(0).take(tempOutRange.size))
+      ImageFileSeqOut(tempOut, spec = specOut, in = sig, indices = tempOutRangeGE)
+      Progress(Frames(sig) / (frameSize.toLong * tempOutRange.size), Metro(frameSize), label = "write")
     }
 
     val ctl = Control(cfg)
