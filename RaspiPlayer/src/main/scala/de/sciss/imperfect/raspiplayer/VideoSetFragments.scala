5894358eb9d15da528a9233bbceebcfc5a17b6f1
Hanns Holger Rutz <contact@sciss.de>
2016-11-26 22:49:27 +0100

    ...

--- RaspiPlayer/src/main/scala/de/sciss/imperfect/raspiplayer/VideoSetFragments.scala
+++ RaspiPlayer/src/main/scala/de/sciss/imperfect/raspiplayer/VideoSetFragments.scala
@@ -22,17 +22,19 @@ object VideoSetFragments extends VideoSet {
 
   private[this] final val indicesExt: Vec[Int] = 0 until 64
 
-  private[this] final val nameFmt = "fragments/fragment-%d.mp4"
+  private[this] final val nameFmt  = "fragments/fragment-%d.mp4"
+  private[this] final val nameFmtS = "fragments/fragment-s-%d.mp4"
 
   def select()(implicit random: Random): Vec[Play] = {
     import Util._
 
     val indices   = random.shuffle(indicesExt).take(IndicesIn.size)
     val cmd       = indices.zipWithIndex.map { case (vidIdx, screenIdx) =>
-      val file    = nameFmt.format(vidIdx + 1)
+      val screen  = Screen(screenIdx)
+      val fmt     = if (screen.isHanging) nameFmt else nameFmtS
+      val file    = fmt.format(vidIdx + 1)
       val fadeIn  = rrand(3.0, 4.5).toFloat
       val fadeOut = rrand(3.0, 4.5).toFloat
-      val screen  = Screen(screenIdx)
       val orient  = choose(screen.orientations)
       Play(file = file, delay = 0f, start = 0f, duration = duration, fadeIn = fadeIn, fadeOut = fadeOut,
         orientation = orient)
